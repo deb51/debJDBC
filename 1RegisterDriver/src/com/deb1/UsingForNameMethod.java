61218883df951419e8204c691ffec48523558260
debasish <debasish@gmail.com>
2021-02-18 16:44:02 +0530

    Register driver demo

--- 1RegisterDriver/src/com/deb1/UsingForNameMethod.java
+++ 1RegisterDriver/src/com/deb1/UsingForNameMethod.java
@@ -7,16 +7,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UsingForNameMethod {
-public static void main(String[] args) throws ClassNotFoundException, SQLException {
-	
-	Class.forName("oracle.jdbc.driver.OracleDriver");
-	
-	Connection conn=DriverManager.getConnection("jdbc:oracle:thin:1521:orcl","scott","tiger");	
-	
-	Statement st=conn.createStatement();
-	ResultSet rs=st.executeQuery("select * from java14");
-	while(rs.next()) {
-		System.out.println(rs.getInt(1)+"==="+rs.getString(2));
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:1521:orcl", "scott", "tiger");
+
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("select * from java14");
+		while (rs.next()) {
+			System.out.println(rs.getInt(1) + "===" + rs.getString(2));
+		}
+		conn.close();
 	}
-}
 }
