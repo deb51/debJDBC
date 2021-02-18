9343700288c4e49724cb7771873dabd5debc9bfd
debasish <debasish@gmail.com>
2021-02-18 16:45:52 +0530

    Registering Driver

---
+++ 1RegisterDriver/src/com/deb1/UsingRegisterDriverMethod.java
@@ -0,0 +1,27 @@
+package com.deb1;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import oracle.jdbc.driver.OracleDriver;
+
+public class UsingRegisterDriverMethod {
+	public static void main(String[] args) throws SQLException {
+		Driver driver = new OracleDriver();
+		DriverManager.registerDriver(driver);
+
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "scott", "tiger");
+
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("select * from java14");
+		while (rs.next()) {
+			System.out.println(rs.getInt(1) + "===" + rs.getString(2));
+		}
+		conn.close();
+
+	}
+}
